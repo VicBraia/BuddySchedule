ec81bc107090e2aa456303bbb96bd1be514fab11
Victória Braia <victoriabraia@gmail.com>
2018-08-22 23:26:49 -0300

    title, table and month position in monthview is responsive. I need to do the same to font size in month and pills sizes

--- public/calendar/monthView.controller.js
+++ public/calendar/monthView.controller.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     angular
@@ -6,7 +6,7 @@
         .controller('MonthViewCtrl', MonthViewCtrl);
 
 
-        MonthViewCtrl.$inject = ['$scope', '$state', 'EventFactory'];
+    MonthViewCtrl.$inject = ['$scope', '$state', 'EventFactory'];
 
     /* @ngInject */
     function MonthViewCtrl($scope, $state, EventFactory) {
@@ -24,7 +24,7 @@
         vm.shownWeekDay = {};
 
         vm.currentView = 0;
-        vm.currentViewList = ['Month','Week', 'Day'];
+        vm.currentViewList = ['Month', 'Week', 'Day'];
 
         vm.monthList = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
         vm.weekDayList = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
@@ -33,98 +33,89 @@
 
         vm.shownDay = vm.currentDay;
         vm.shownYear = vm.currentYear;
-        vm.shownWeekDay = vm.weekDayList[vm.currentWeekDay];  
-        vm.shownMonth = vm.currentMonth - 1; 
-        vm.monthWeeks = [];    
-        
-        vm.changeMonth = changeMonth;
-        vm.abbrev = abbrev;
+        vm.shownWeekDay = vm.weekDayList[vm.currentWeekDay];
+        vm.shownMonth = vm.currentMonth - 1;
+        vm.monthWeeks = [];
 
+        vm.changeMonth = changeMonth;
+        vm.weekList = {};
         activate();
-        
+
         function activate() {
-            vm.monthWeeks = initializeMonth(vm.shownMonth);
-            vm.weekList = vm.weekDayList;
+            vm.monthWeeks = initializeMonth(vm.shownMonth);      
+            if ($(window).width() < 960) {
+                vm.weekList = vm.abbrevWeekDayList;
+             }
+             else {
+                vm.weekList = vm.weekDayList;
+            }        
         }
 
-        function abbrev(){
-            alert(outerwidth)
-            if(window.outerwidth<436){
+        
+        $(window).on('resize', function () {            
+            if ($(window).width() < 960) {
                 vm.weekList = vm.abbrevWeekDayList;
-            }
-            else{
+             }
+             else {
                 vm.weekList = vm.weekDayList;
             }
-        }
+            console.log(vm.weekList);
+            
+        });
 
-        function initializeMonth(month){
+        function initializeMonth(month) {
             var daysToBeShown = [];
             var date;
-            var day = new Date (vm.currentYear, month, 1);
+            var day = new Date(vm.currentYear, month, 1);
             var weekDay = day.getDay();
 
-            while(weekDay != 0){ //get dates from previous month that appear in the first week of the current month
-                date = new Date (vm.currentYear, month, -weekDay+1);
+            while (weekDay != 0) { //get dates from previous month that appear in the first week of the current month
+                date = new Date(vm.currentYear, month, -weekDay + 1);
                 date = date.getDate();
                 daysToBeShown.push(date);
                 weekDay--;
             }
 
-            while(day.getMonth() == month){ //get current month's dates
-                date =  day.getDate();
+            while (day.getMonth() == month) { //get current month's dates
+                date = day.getDate();
                 day.setDate(day.getDate() + 1);
                 daysToBeShown.push(date);
             }
 
-            for(var i = day.getDay(); i<=6; i++){
-                date =  day.getDate();
+            for (var i = day.getDay(); i <= 6; i++) {
+                date = day.getDate();
                 day.setDate(day.getDate() + 1);
                 daysToBeShown.push(date);
             }
 
             var calendarWeeks = [];
-            for(var i = 0; i < daysToBeShown.length; i+= 7){
-                var tmp = daysToBeShown.slice(i, i+7);
+            for (var i = 0; i < daysToBeShown.length; i += 7) {
+                var tmp = daysToBeShown.slice(i, i + 7);
                 calendarWeeks.push(tmp);
             }
             return calendarWeeks;
         }
 
-        function changeMonth(value){
-            switch(value){
-                case(-1):
+        function changeMonth(value) {
+            switch (value) {
+                case (-1):
                     vm.shownMonth--;
-                    if(vm.shownMonth < 0){
+                    if (vm.shownMonth < 0) {
                         vm.shownYear--;
                         vm.shownMonth = 11;
                     }
-                    vm.monthWeeks = initializeMonth(vm.shownMonth); 
-                break;
-                case(1):
+                    vm.monthWeeks = initializeMonth(vm.shownMonth);
+                    break;
+                case (1):
                     vm.shownMonth++;
-                    if(vm.shownMonth > 11){
+                    if (vm.shownMonth > 11) {
                         vm.shownYear++;
                         vm.shownMonth = 0;
                     }
-                    vm.monthWeeks = initializeMonth(vm.shownMonth); 
-                break;
-            }
-        }
-
-        function changeView(option){
-            switch(option){
-                case 1:
-                    //Month
-                break;
-                case 2:
-                    //Week
-                break;
-                case 3:
-                    //Day
-                break;
+                    vm.monthWeeks = initializeMonth(vm.shownMonth);
+                    break;
             }
         }
     }
 })();
 
-        
